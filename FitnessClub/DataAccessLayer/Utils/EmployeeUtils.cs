f52d3e14b0532d1980cff3f5d25b6b5e165d1863
Rapa Erik <rapa.csek@gmail.com>
2018-05-18 16:59:54 +0300

    Login view+ Controller + Authentication

---
+++ FitnessClub/DataAccessLayer/Utils/EmployeeUtils.cs
@@ -0,0 +1,28 @@
+﻿using DataAccessLayer.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Utils
+{
+    public class EmployeeUtils
+    {
+        public static bool AuthenticationEmployee(string name, string pwd)
+        {
+            int result = 0;
+            using (var ctx=new NorthwindContext())
+            {
+                var query = from emp in ctx.Employees
+                            where emp.Name==name &&
+                                   emp.Password==pwd &&
+                                   emp.IsDeleted == false
+                            select emp.Id;
+                result = query.FirstOrDefault();
+            }
+            return Convert.ToBoolean(result);
+
+        }
+    }
+}
