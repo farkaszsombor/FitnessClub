1497ad3308cacd01fff21457df7b2dd27c004f8f
Timi <Timi@DESKTOP-V19GCVS>
2018-05-19 15:02:10 +0300

    Created TicketUtils and starting Event and RoomUtils

---
+++ FitnessClub/DataAccessLayer/Utils/RoomUtils.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer.Contexts;
+using DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Utils
+{
+    public class RoomUtils
+    {
+        public static bool InsertRoom(string name)
+        {
+            using (var ctx = new NorthwindContext())
+            {
+                ctx.Rooms.Add(new Room {Name = name});
+                return true;
+            }
+        }
+    }
+}
